85f9b4a65e7514509bfa0a43724c72d26ccdb857
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-05-06 17:26:26 -0300

    adding a test insert into database

--- 2_BackEndLayer/go/main.go
+++ 2_BackEndLayer/go/main.go
@@ -8,15 +8,28 @@ import (
 )
 
 func main() {
+
 	fmt.Println("Go MySQL Connection")
 
+	// esta linea no funciono porque inclui el password como en el tutorial,
+	// pero mi base dedatos no usa password,
+	// pero la dejo comentada para futuras referencias
+	// db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/agenda")
 	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/agenda")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer db.Close()
+	insertString := "INSERT INTO agenda (`full_names`,`direction`,`house_phone`,`mobile_phone`,`email`)VALUES ('marcos rosi','giñu 9486',45693873,11563564325,'marcos.rosi@company.com')"
 
-	fmt.Println("Successfully connected to MySQL database")
+	insert, err := db.Query(insertString)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+
+	fmt.Println("Successfully incerted database")
 }
