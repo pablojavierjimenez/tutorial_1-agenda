d6e9f346b71841c725a37dac835dc95aaacac570
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-05-12 00:09:37 -0300

    adding dependenci and connect with mysql

---
+++ 2_BackEndLayer/nodejs/READM.md
@@ -0,0 +1,5 @@
+# NodeJS
+
+
+
+[video of MySQL connection](https://www.youtube.com/watch?v=Eb-jstRRpcA)
---
+++ 2_BackEndLayer/nodejs/db.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql')
+
+// Create DB connection
+var db = mysql.createConnection({
+  host:"127.0.0.1",
+  user:"root",
+  password:"",
+  database:"agendaDB"
+})
+
+// $port = "3306";
+db.connect(function(err){
+  if(err) throw err;
+  console.log("connection success!!")
+})
