0391f4e89f3a7fd8d374599c77f39d89d6d5358b
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-05-12 18:57:41 -0300

    create server done

--- 2_BackEndLayer/nodejs/index.js
+++ 2_BackEndLayer/nodejs/index.js
@@ -1,7 +1,25 @@
+var http = require("http")
 var _db = require("./db")
 
-_db.query('SELECT * FROM persons LIMIT 10', function(err, result, fields){
-    if(err) throw err;
 
-    console.log("results: ", result);
+var homeHandler = function (req, res) {
+  console.log("hola sol el server de la home")
+
+  res.end("Hola soy la home")
+}
+
+
+
+
+_db.query('SELECT * FROM persons LIMIT 10', function(err, result, fields){
+  if(err) throw err;
+
+  console.log("results: ", result);
 })
+
+_db.end()
+
+// Creamos el servidor
+var server = http.createServer(homeHandler);
+
+server.listen(7080)
